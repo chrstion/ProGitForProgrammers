33c4e877978a29b4d6af42d8d84a4dd7c341efff
Christian TAYORO <christ_andre@hotmail.fr>
2022-03-03 14:29:46 -0500

    Create Movie.cs
    Add Title and Actors

---
+++ ProGitForProgrammers/ProGitForProgrammers.console/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProGitForProgrammers.console
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public List<string> Actors { get; set; }
+    }
+}
