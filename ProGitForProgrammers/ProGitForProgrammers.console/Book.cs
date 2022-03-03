ac9e676713a9e0a64903c03ac30dbb75899c8b04
Christian TAYORO <christ_andre@hotmail.fr>
2022-03-03 14:19:01 -0500

    New class Book added

---
+++ ProGitForProgrammers/ProGitForProgrammers.console/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProGitForProgrammers.console
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+    }
+}
