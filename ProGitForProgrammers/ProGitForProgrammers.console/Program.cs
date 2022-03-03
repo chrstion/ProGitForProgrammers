da4aa0b4ebf6c65ef2ceb5bc82944c8411f86833
Christian TAYORO <christ_andre@hotmail.fr>
2022-03-03 12:29:05 -0500

    Add information line

--- ProGitForProgrammers/ProGitForProgrammers.console/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers.console/Program.cs
@@ -7,6 +7,8 @@ namespace ProGitForProgrammers.console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("I just added this in Visual Studio");
         }
     }
 }
