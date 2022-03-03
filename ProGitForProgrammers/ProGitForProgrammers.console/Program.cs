dea72993b997223606f09a2e7429885a3086bec5
Christian TAYORO <christ_andre@hotmail.fr>
2022-03-03 13:13:40 -0500

    Delete spaces between lines from program.cs

--- ProGitForProgrammers/ProGitForProgrammers.console/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers.console/Program.cs
@@ -7,10 +7,9 @@ namespace ProGitForProgrammers.console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
             Console.WriteLine("I just added this in Visual Studio");
-
             Console.WriteLine("I just added this in the command line repo");
+            Console.WriteLine("This line added to show the staging area");
         }
     }
 }
